e5dff9e23cb2902ea1df1e05ff933dcc504e2fd2
PisethMao <pisethmao2002@gmail.com>
2026-05-06 09:35:54 +0700

    basic input and output with calculator practice

---
+++ basic-input-and-output/concatenated-string.py
@@ -0,0 +1,8 @@
+first_name = "Piseth"
+last_name = "Mao"
+full_name = first_name + " " + last_name
+print("Hello, " + full_name)
+
+words = ["Python", "is", "awesome!!!"]
+result = " ".join(words)
+print(result)
